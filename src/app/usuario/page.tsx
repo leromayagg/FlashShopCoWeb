a4313e9a44b0913513d92b8d986d1bf6c5146df4
Logon Aluno <logonrmlocal@fiap.com.br>
2024-04-10 10:32:30 -0300

    Usuario page

--- src/app/usuario/page.tsx
+++ src/app/usuario/page.tsx
@@ -5,7 +5,8 @@ export default function Usuario() {
   return (
     <main className="flex min-h-screen flex-col items-center">
       <NavBar active="usuario"/>
-      <h2>Usuario</h2>
+      <h2 className="flex font-bold mt-20 justify-center text-4xl md:text-5xl lg:text-6xl">Usuario</h2>
+      <h3>Incompleto</h3>
     </main>
   );
 }
